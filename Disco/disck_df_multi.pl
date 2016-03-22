5114d1f441d5e42ca8aef62384db2ad7bc64c3e6
Jorge <jorge@localhost.localdomain>
2016-03-22 19:05:33 -0300

    probando getoptions perl

--- Disco/disck_df_multi.pl
+++ Disco/disck_df_multi.pl
@@ -1,39 +1,13 @@
 #!/usr/bin/perl
-$PATH="$ENV{'PWD'}";
-#$por_warning=$ARGV[0];
-#$por_critical=$ARGV[1];
-#
-open(MIFICH,">$PATH/discosdf.cmd");
-#
-open(IN, "df -h |");
-##Size  Used Avail Use% Mounted on
-## 8,1G  4,4G  3,3G  57% /
-#
-while(<IN>){
-	chomp();
-	if (/\s(\d+[\.\,]\w+|\d+\w+|\d+)\s+(\d+[\.\,]\w+|\d+\w+|\d+)\s+(\d+[\.\,]\w+|\d+\w+|\d+)\s+(\d+%)\s+([\/A-Za-z0-9]+)/){
-	$size=$1;
-	$used=$2;
-	$use=$4;
-	$avail=$3;
-	$disk=$5;
-	if($disk=~ ".*mysql"){ $disk="mysql"}
-	print MIFICH "command[$disk]=$PATH/check_df_multi.pl $size $used $avail $use $por_warning $por_critical\n";
-	}
+use strict;
+use Getopt::Long;
+
+our($dir_check_multi);
+GetOptions("dir_check_multi|dcm:s" => \$dir_check_multi,
+			"h|H|help");
+
+if(!$dir_check_multi){
+	$dir_check_multi="/usr/local/nagios/libexec/";
+}else{
+	print $dir_check_multi;
 }
-close(IN);
-print MIFICH "\n\n";
-print MIFICH "state [OK] = (COUNT(CRITICAL) < 0)\n";
-print MIFICH "state [WARNING] = (COUNT(WARNING) > 0) \n";
-print MIFICH "state [CRITICAL] = (COUNT(CRITICAL) > 0) or (COUNT(WARNING) > 1)\n ";
-close MIFICH;
-$check=`/usr/local/nagios/libexec/check_multi -f $PATH/discosdf.cmd`;
-print "$check";
-if($check=~/^([A-Z]+) -/){
-if( $1 eq "WARNING"){exit(1)}
-elsif($1 eq "OK"){exit(0)}
-elsif($1 eq "CRITICAL"){exit(2)}
-else{exit(3)}
-																			}
-#
-#
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Monitoreo
+***** Pre-requisitos ****
+1.- check_multi (http://my-plugin.de/wiki/projects/check_multi/start)
