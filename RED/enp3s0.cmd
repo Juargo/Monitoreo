0e397dda9e1c623e70ac416d5e00ce699b14e689
Jorge <jorge@localhost.localdomain>
2016-04-04 16:43:01 -0300

    mejoras de nombres

--- RED/enp3s0.cmd
+++ RED/enp3s0.cmd
@@ -1,2 +1,2 @@
-command[Recibidos]=/home/jorge/Documentos/Monitoreo/RED/check_trafico_interfaz.pl 34700108 0
-command[Transmitidos]=/home/jorge/Documentos/Monitoreo/RED/check_trafico_interfaz.pl 935019 0
+command[Recibidos]=/home/jorge/Documentos/Monitoreo/RED/check_trafico_interfaz.pl 67617 0
+command[Transmitidos]=/home/jorge/Documentos/Monitoreo/RED/check_trafico_interfaz.pl 12560 0
--- RED/trafico_interfaces.pl
+++ RED/trafico_interfaces.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl
+use Cwd 'abs_path';
 
+$DIR=substr(abs_path($0),0,-22);
 
 open (IN, "cat /proc/net/dev |awk '{print \$1,\$2,\$4,\$10,\$12}'|grep :|sed 's/://g'|");
 
-open (DIFF,"cat /home/jorge/Documentos/Monitoreo/RED/pass.txt|");
+open (DIFF,"cat $DIR/pass.txt|");
 
 while (<DIFF>){
 	 chomp();
@@ -15,8 +17,8 @@ while (<DIFF>){
 }
 
 close DIFF;
- `rm -f /home/jorge/Documentos/Monitoreo/RED/pass.txt`;
-open (PFILE,">/home/jorge/Documentos/Monitoreo/RED/trafico_interfaz.cmd");
+ `rm -f $DIR/pass.txt`;
+open (PFILE,">$DIR/trafico_interfaces.cmd");
 while (<IN>){
 	chomp();
 	($interface,$rbyts,$rerr,$tbyts,$terr)=split(" ",$_);
@@ -26,20 +28,20 @@ while (<IN>){
         $interfaces{$interface}{'transmitidos'}=$tbyts-$interfaces_pass{$interface}{'transmitidos'};
         $interfaces{$interface}{'transmitidoserr'}=$terr-$interfaces_pass{$interface}{'transmitidoserr'};
 	
-	open(SFILE,">/home/jorge/Documentos/Monitoreo/RED/$interface.cmd");
+	open(SFILE,">$DIR/$interface.cmd");
 	
-	print PFILE "command[$interface]=/usr/local/nagios/libexec/check_multi -f /home/jorge/Documentos/Monitoreo/RED/$interface.cmd\n";
-	print SFILE "command[Recibidos]=/home/jorge/Documentos/Monitoreo/RED/check_trafico_interfaz.pl $interfaces{$interface}{'recibidos'} $interfaces{$interface}{'recibidoserr'}\n";
-	print SFILE "command[Transmitidos]=/home/jorge/Documentos/Monitoreo/RED/check_trafico_interfaz.pl $interfaces{$interface}{'transmitidos'} $interfaces{$interface}{'transmitidoserr'}\n";
+	print PFILE "command[$interface]=/usr/local/nagios/libexec/check_multi -f $DIR/$interface.cmd\n";
+	print SFILE "command[Recibidos]=$DIR/check_trafico_interfaz.pl $interfaces{$interface}{'recibidos'} $interfaces{$interface}{'recibidoserr'}\n";
+	print SFILE "command[Transmitidos]=$DIR/check_trafico_interfaz.pl $interfaces{$interface}{'transmitidos'} $interfaces{$interface}{'transmitidoserr'}\n";
 	
 	close SFILE;
-	open(DIFFN,">>/home/jorge/Documentos/Monitoreo/RED/pass.txt");
+	open(DIFFN,">>$DIR/pass.txt");
 	
 	print DIFFN "$interface $rbyts $rerr $tbyts $terr\n";
 	close DIFFN;
 }
 close IN;
 close PFILE;
-$check=`/usr/local/nagios/libexec/check_multi  -f /home/jorge/Documentos/Monitoreo/RED/trafico_interfaz.cmd`;
+$check=`/usr/local/nagios/libexec/check_multi  -f $DIR/trafico_interfaces.cmd`;
 print$check;
 
